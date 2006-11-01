0680f79ebfe1e50b170be925d0fb129d0174c121
particle <particle@cpan.org>
2006-11-01 00:13:04 +0000

    Import of PARTICLE/Bundle-Parrot-0.4.6_01 from CPAN.
    gitpan-cpan-distribution: Bundle-Parrot
    gitpan-cpan-version:      0.4.6_01
    gitpan-cpan-path:         PARTICLE/Bundle-Parrot-0.4.6_01.zip
    gitpan-cpan-author:       PARTICLE
    gitpan-cpan-maturity:     developer

---
+++ lib/Bundle/Parrot.pm
@@ -0,0 +1,41 @@
+package Bundle::Parrot;
+our $VERSION = '0.4.6_01';
+
+1;
+__END__
+=head1 NAME
+
+Bundle::Parrot - Bundle of modules required for developing and testing Parrot
+
+=head1 SYNOPSIS
+
+  C<perl -MCPAN -e'install Bundle::Parrot'>
+
+=head1 CONTENTS
+
+Digest::Perl::MD5      1.5
+File::Which            0.05
+Module::Build
+Parse::RecDescent
+Pod::Find
+Pod::Simple
+Pod::Escapes
+Test::Pod
+Test::Simple           0.60
+Text::Balanced
+Test::TAP::HTMLMatrix
+Test::Base
+
+=head1 DESCRIPTION
+
+These modules are required for developing and testing Parrot.
+
+=head1 AUTHOR
+
+Jerry Gay a.k.a. particle, E<lt>jerry.gay+parrot@gmail.comE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2006 The Perl Foundation.  All Rights Reserved.
+
+=cut
